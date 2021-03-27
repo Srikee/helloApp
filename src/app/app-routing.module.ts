9385f694e04cf8480baba6191f4afeb8b32368be
Srikee <kee19384@gmail.com>
2021-03-27 08:59:14 +0700

    save

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'prayut',
+    loadChildren: () => import('./prayut/prayut.module').then( m => m.PrayutPageModule)
   },
+
 ];
 
 @NgModule({
